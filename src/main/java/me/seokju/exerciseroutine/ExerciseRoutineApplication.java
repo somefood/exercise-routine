57f00bb6d14f6be503937d9c15912df2245c0fec
Seokju Hong <iamseokjuhong@gmail.com>
2023-05-30 23:51:52 +0900

    project init

---
+++ src/main/java/me/seokju/exerciseroutine/ExerciseRoutineApplication.java
@@ -0,0 +1,13 @@
+package me.seokju.exerciseroutine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseRoutineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciseRoutineApplication.class, args);
+    }
+
+}
